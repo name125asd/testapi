252f381a014bf9d665dc3ab8b63e16e6bf3baffa
name125asd <name125asd@gmail.com>
2023-10-25 09:39:12 +0700

    first commit

---
+++ WebApplication5/Models/Request.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class Request
+    {
+        public string user;
+
+        public string pass;
+
+    }
+}
